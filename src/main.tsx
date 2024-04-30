8fd2be5e39b5099829d2c6acb1cf1e4db58279ec
Luka Bulatovic <luka.bulatovic@condor-business.com>
2024-04-30 15:02:49 +0200

    removed react.Strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+    // removed React.StrictMode because of double firing of useEffect in developement mode
     <App />
-  </React.StrictMode>,
 )
